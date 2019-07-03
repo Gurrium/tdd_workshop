7220999439285c1d07e8c5d4f693b59489b0a6c7
taira120 <taira1999120@gmail.com>
2019-07-03 12:59:01 +0900

    write todo list

---
+++ fizz_buzz.rb
@@ -0,0 +1,13 @@
+# - [ ] 数を文字列に変換する
+# - [ ] 3の倍数のときはfizzに変換する
+# - [ ] 5の倍数のときはbuzzに変換する
+# - [ ] 3, 5両方の倍数のときはfizzbuzzに変換する
+#
+# - [ ] 1から100までの数
+# - [ ] プリントする
+# - [ ]
+
+require 'minitest/autorun'
+
+class SolverTest < Minitest::Test
+end
