d19bf8543d6c3d069e1a9be8d4ee2ca87275b823
taira120 <taira1999120@gmail.com>
2019-07-03 13:26:08 +0900

    1st green

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,4 +1,5 @@
 # - [ ] 数を文字列に変換する
+#   - [ ] 1を渡すと文字列"1"に変換する
 # - [ ] 3の倍数のときはfizzに変換する
 # - [ ] 5の倍数のときはbuzzに変換する
 # - [ ] 3, 5両方の倍数のときはfizzbuzzに変換する
@@ -9,5 +10,23 @@
 
 require 'minitest/autorun'
 
-class SolverTest < Minitest::Test
+class FizzBuzz
+  def initialize
+
+  end
+
+  def stringify(i)
+    '1'
+  end
+end
+
+class FizzBuzzTest < Minitest::Test
+  def test_convert_1_to_string_1
+    # 準備
+    fizz_buzz = FizzBuzz.new
+    # 実行
+    actual = fizz_buzz.stringify(1)
+    # 検証
+    assert_equal '1', actual
+  end
 end
