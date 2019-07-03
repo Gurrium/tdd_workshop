399070bbb2e0f4b6348fa875c098bb1f9f0ce641
taira120 <taira1999120@gmail.com>
2019-07-03 13:50:23 +0900

    hoge

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -16,8 +16,10 @@ class FizzBuzz
   def initialize; end
 
   def stringify(num)
-    if(num == 3)
+    if(num % 3 == 0)
       'fizz'
+    elsif(num % 5 == 0)
+      'buzz'
     else
       num.to_s
     end
@@ -25,27 +27,31 @@ class FizzBuzz
 end
 
 class FizzBuzzTest < Minitest::Test
+  def setup
+    @fizz_buzz = FizzBuzz.new
+  end
+
   def test_convert_1_to_string_1
-    # 準備
-    fizz_buzz = FizzBuzz.new
-    # 実行
-    # 検証
-    assert_equal '1', fizz_buzz.stringify(1)
+    assert_equal '1', @fizz_buzz.stringify(1)
   end
 
   def test_convert_2_to_string_2
-    # 準備
-    fizz_buzz = FizzBuzz.new
-    # 実行
-    # 検証
-    assert_equal '2', fizz_buzz.stringify(2)
+    assert_equal '2', @fizz_buzz.stringify(2)
   end
 
   def test_convert_3_to_string_fizz
-    # 準備
-    fizz_buzz = FizzBuzz.new
-    # 実行
-    # 検証
-    assert_equal 'fizz', fizz_buzz.stringify(3)
+    assert_equal 'fizz', @fizz_buzz.stringify(3)
+  end
+
+  def test_convert_multiple_of_3_to_string_fizz
+    assert_equal 'fizz', @fizz_buzz.stringify(6)
+  end
+
+  def test_convert_5_to_string_buzz
+    assert_equal 'buzz', @fizz_buzz.stringify(5)
+  end
+
+  def test_convert_multiple_of_5_to_string_fizz
+    assert_equal 'buzz', @fizz_buzz.stringify(10)
   end
 end
